c9a18c556b63341f711c4df70483bec549926a16
Arthursolis <68565925+Arthursolis@users.noreply.github.com>
2020-09-09 11:10:19 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <div className="App">
+      <h1>Doe, Jane</h1>
+      <ul>
+        <li>Age : 45</li>
+        <li>hair Color: Black</li>
+      </ul>
+  </div>
+      <div className="App">
+      <h1>Smith, John</h1>
+      <ul>
+        <li>Age : 88</li>
+        <li>hair Color: Brown</li>
+      </ul>
+  </div>
+      <div className="App">
+      <h1>Fillmore, Millard</h1>
+      <ul>
+        <li>Age : 50</li>
+        <li>hair Color: Brown</li>
+      </ul>
+    </div>
+        <div className="App">
+        <h1>Smith, Maria</h1>
+        <ul>
+          <li>Age : 62</li>
+          <li>hair Color: Brown</li>
+        </ul>
+    </div>
+  );
+}
+
+export default App;
